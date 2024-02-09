416125a5052f8d2084a282cd1df3d7709b6180fd
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-09 23:21:28 +0100

    Complete Navbar setup with Raleway font integration

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/logo.png";
+const Navbar = () => {
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo" style={{ width: "80px" }} />
+      </div>
+      {/* menu */}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/* hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+      {/* mobile menu */}
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
+      {/* social icons */}
+      <div className="hidden"></div>
+    </div>
+  );
+};
+
+export default Navbar;

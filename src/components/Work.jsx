68aeb7771d97149f4bd7e58502cd7448c87b8498
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-12 14:30:14 +0100

    Add First Real Project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import workImg from "../assets/projects/workImg.jpeg";
 import realEstate from "../assets/projects/realestate.jpg";
+import netflixImg from "../assets/projects/netflix.jpg";
 
 const Work = () => {
   return (
@@ -15,20 +16,20 @@ const Work = () => {
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${netflixImg})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                Netflix Clone App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://main--sp-react-netflix-clone.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/sasa-potezica/netflix-clone">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>

301e2d18d17295a253c026fd9748bb3328f09fff
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-16 17:52:47 +0100

    Update Work.jsx and Add New Project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -104,15 +104,23 @@ const Work = () => {
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+                HomeLand
               </span>
               <div className="pt-8 text-center">
-                <a target="_blank" rel="noopener noreferrer" href="/">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://sp-real-estate.netlify.app/"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a target="_blank" rel="noopener noreferrer" href="/">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/sasa-potezica/real-estate"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>

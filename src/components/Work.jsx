6950f7462942801c9b504f056a324c2ab1d83b5c
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-02-22 10:51:00 +0100

    Update Work.jsx with Images and Descriptions

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import netflixImg from "../assets/projects/netflix.jpg";
 import cryptoImg from "../assets/projects/CryptoBase.jpg";
 import HooBank from "../assets/projects/HooBank.jpg";
 import Fitness from "../assets/projects/Fitness.jpg";
-import LeetCode from "../assets/projects/LeetCode.jpg";
+import ecom from "../assets/projects/ecom.jpg";
 
 const Work = () => {
   return (
@@ -25,6 +25,10 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 Netflix Clone
+                <p className="text-sm -mb-10 p-4">
+                  Dive into seamless streaming with this project, featuring
+                  dynamic UI design and real-time data management.
+                </p>
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -55,6 +59,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 CryptoBase
+                <p className="text-sm -mb-10 p-4">
+                  Your go-to app for exploring and analyzing crypto coin data,
+                  with the added convenience of saving preferences to your
+                  account.
+                </p>
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -85,6 +94,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 HooBank
+                <p className="text-sm -mb-10 p-4">
+                  Experience the future of banking with HooBank, the
+                  next-generation payment method offering seamless transactions
+                  and advanced financial management features.
+                </p>
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -115,6 +129,11 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 HomeLand
+                <p className="text-sm -mb-10 p-4">
+                  Discover your dream property with HomeLand, a user-friendly
+                  website offering comprehensive real estate listings and
+                  intuitive search tools for finding your perfect home.
+                </p>
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -139,12 +158,42 @@ const Work = () => {
             </div>
           </div>
           <div
+            style={{ backgroundImage: `url(${ecom})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Notice!
+                <p className="text-sm -mb-10 p-4">
+                  Still under Construction...
+                </p>
+              </span>
+              <div className="pt-8 text-center">
+                <a target="_blank" rel="noopener noreferrer" href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
             style={{ backgroundImage: `url(${Fitness})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 EvoGym
+                <p className="text-sm -mb-10 p-4">
+                  Explore dynamic gym membership options on EvoGym's website,
+                  designed to help you reach your fitness goals with ease and
+                  convenience.
+                </p>
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -161,28 +210,6 @@ const Work = () => {
                   rel="noopener noreferrer"
                   href="https://github.com/sasa-potezica/evo-gym"
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${LeetCode})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                LeetCode Clone
-              </span>
-              <div className="pt-8 text-center">
-                <a target="_blank" rel="noopener noreferrer" href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
